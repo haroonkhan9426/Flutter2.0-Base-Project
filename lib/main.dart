e2bbaa1672b149d306d58dad1da8ddeb2371edaf
Haroon khan <haroonkhan9426@gmail.com>
2021-04-30 15:34:36 +0500

    Localization done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('ar')],
+        path: 'assets/translations', // Translations path
+        fallbackLocale: Locale('ar'),
+        child: MyApp()),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -18,9 +36,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home Page'));
+    return Scaffold(
+        body: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('hello'.tr()),
+        context.locale.languageCode == 'en'
+            ? ElevatedButton(
+                child: Text('Change to Arabic'),
+                onPressed: () {
+                  context.setLocale(Locale('ar'));
+                  // setState(() {
+                  //   context.setLocale();
+                  // });
+                },
+              )
+            : ElevatedButton(
+                child: Text('Change to English'),
+                onPressed: () {
+                  context.setLocale(Locale('en'));
+                },
+              )
+      ],
+    )));
   }
 }
